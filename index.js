4bd8abab22abba1f7da4f37e749e0d34cdcecc8f
Connor Henn <connor.henn@yahoo.com>
2022-07-20 20:00:49 -0700

    commit tree 72c98b0a73a77470dc053b0ff8e8ecfc259790ba

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    const firstTwoArray = drivers.slice(0, 2);
+    return firstTwoArray;
+}
+
+const returnLastTwoDrivers = function(drivers){
+    const lastTwoArray = drivers.slice(drivers.length-2, drivers.length);
+    return lastTwoArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return multiplier * fare;
+    };
+    
+}
+function fareDoubler(){
+const fareDoubled = createFareMultiplier(2);
+return fareDoubled;
+}
+
+function fareTripler(){
+const fareTripled = createFareMultiplier(3);
+return fareTripled;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driverFunction){
+    return driverFunction(arrayOfDrivers);}
